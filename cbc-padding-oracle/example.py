0814565165db29c01a16614d9a71fe0004f0a2eb
Diego F. Aranha <dfaranha@gmail.com>
2022-11-07 11:25:58 +0100

    Improve certificate validation.

--- cbc-padding-oracle/example.py
+++ cbc-padding-oracle/example.py
@@ -10,7 +10,7 @@ def test_systems_security(base_url):
     res = requests.get(f'{base_url}/', verify="ca.pem")
     ciphertext = bytes.fromhex(res.cookies['authtoken'])
     print(f'[+] received ciphertext: {ciphertext.hex()}')
-    res = requests.get(f'{base_url}/check/', cookies={'authtoken': ciphertext.hex()}, verify=False)
+    res = requests.get(f'{base_url}/check/', cookies={'authtoken': ciphertext.hex()}, verify="ca.pem")
     print(f'[+] done:\n{res.text}')
 
 if __name__ == '__main__':
